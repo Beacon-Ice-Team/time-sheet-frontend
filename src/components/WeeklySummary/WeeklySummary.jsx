24ecf22cea90947ecad1f001de5f243b9db5125b
WentaoHu <wentaohu1017@gmail.com>
2020-04-08 22:55:41 -0400

    weekly summary frontend

--- src/components/WeeklySummary/WeeklySummary.jsx
+++ src/components/WeeklySummary/WeeklySummary.jsx
@@ -1,10 +1,64 @@
 import React from 'react';
+import Table from 'react-bootstrap/Table'
 
 class WeeklySummary extends React.Component {
-    render(){
+    constructor(props) {
+        super(props)
+        this.state = {
+            weeklySummaryRecordList : []
+        };
+    }
+
+    componentDidMount() {
+        const weeklySummaryRequest = {
+            method: 'GET',
+            headers: { 'Content-Type': 'application/json' },
+        }
+
+        fetch('/weekly-summary/get', weeklySummaryRequest)
+            .then(response => response.json())
+            .then(data => this.setState({
+                weeklySummaryRecordList : data.weeklySummaryRecordList
+            }));
+    }
+
+    renderTableData(){
+        console.log(this.state.weeklySummaryRecordList)
+        return this.state.weeklySummaryRecordList.map((weeklySummaryRecord, index) => {
+            const {weekEnding, hours, submissionStatus, approvalStatus, comments} = weeklySummaryRecord
+            return (
+                <tr key={weekEnding}>                     
+                    <td>{weekEnding}</td>
+                    <td>{hours}</td>
+                    <td>{submissionStatus}</td>
+                    <td>{approvalStatus}</td>
+                    <td><a href="/#">Edit</a></td>
+                    <td>{comments}</td>
+                </tr>
+            )
+        })
+    }
+
+    render() {
         return (
             <div>
                 <h1>Summary Page</h1>
+                <Table striped bordered hover>
+                    <thead>
+                        <tr>
+                            <th>WeekEnding</th>
+                            <th>Total hours</th>
+                            <th>Submission Status</th>
+                            <th>Approval Status</th>
+                            <th>Option</th>
+                            <th>Comments</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.renderTableData()}
+                    </tbody>
+                </Table>
+
             </div>
         )
     }
